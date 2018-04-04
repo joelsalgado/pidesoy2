3afada89a7f5abe854833afb21f9969332ffe606
joelsalgado <joelsalgado1302@gmail.com>
2018-04-03 21:54:25 -0500

    cambios PIDESO

--- console/controllers/PidesoController.php
+++ console/controllers/PidesoController.php
@@ -54,7 +54,99 @@ class PidesoController extends Controller
             $colonia = ($data['colonia'] == null ) ? '':$data['colonia'];
             $ext = ($data['numeroExterior'] == null ) ? '':$data['numeroExterior'];
             $int = ($data['numeroInterior'] == null ) ? '':$data['numeroInterior'];
-            $codigo_postal = ($data['codigoPostal'] == null ) ? '':$data['codigoPostal'];
+            $codigo_postal = ($data['codigoPostal'] == null ) ? 50000 :$data['codigoPostal'];
+            $otra_referencia = ($data['otraReferencia'] == null ) ? '' :$data['otraReferencia'];
+
+            $cuantos_habitan = ($data['cuantosHabitan'] == null ) ? 1 :$data['cuantosHabitan'];
+            $personas_0_15 = ($data['personas0a15'] == null ) ? 0 :$data['personas0a15'];
+            $personas_16_17 = ($data['personas16a17'] == null ) ? 0 :$data['personas16a17'];
+            $personas_18_74 = ($data['personas18a74'] == null ) ? 0 :$data['personas18a74'];
+            $personas_65_mas = ($data['personas65oMas'] == null ) ? 0 :$data['personas65oMas'];
+            $tiempo_anios = ($data['tiempoDeHabitacionAnios'] == null ) ? 0 :$data['tiempoDeHabitacionAnios'];
+            $tiempo_meses = ($data['tiempoDeHabitacionMeses'] == null ) ? 0 :$data['tiempoDeHabitacionMeses'];
+
+            switch ($data['laViviendaEs']){
+                case 'COMPARTIDA FAMILIAR':
+                    $vivienda_es = 2;
+                    break;
+                case 'PRESTADA':
+                    $vivienda_es = 3;
+                    break;
+                case 'PROPIA':
+                    $vivienda_es = 1;
+                    break;
+                case 'RENTADA':
+                    $vivienda_es = 4;
+                    break;
+                default:
+                    $vivienda_es = 1;
+            }
+
+            $familias_vivienda = ($data['familiasEnLaVivienda'] == null ) ? 1 :$data['familiasEnLaVivienda'];
+
+            switch ($data['pisoFirme']){
+                case 'SI':
+                    $piso_firme = 1;
+                    break;
+                case 'NO':
+                    $piso_firme = 0;
+                    break;
+                default:
+                    $piso_firme = 0;
+            }
+
+            $piso_material = ($data['materialPiso'] == null ) ? '' :$data['materialPiso'];
+
+            switch ($data['techoFirme']){
+                case 'SI':
+                    $techo_firme = 1;
+                    break;
+                case 'NO':
+                    $techo_firme = 0;
+                    break;
+                default:
+                    $techo_firme = 0;
+            }
+
+            $techo_material = ($data['materialTecho'] == null ) ? '' :$data['materialTecho'];
+
+            switch ($data['muroFirme']){
+                case 'SI':
+                    $muro_firme = 1;
+                    break;
+                case 'NO':
+                    $muro_firme = 0;
+                    break;
+                default:
+                    $muro_firme = 0;
+            }
+
+            $muro_material = ($data['materialMuro'] == null ) ? '' :$data['materialMuro'];
+
+            $habitaciones = ($data['habitaciones'] == null ) ? 1 :$data['habitaciones'];
+
+            switch ($data['aguaPublica']){
+                case 'SI':
+                    $agua_publica = 1;
+                    break;
+                case 'NO':
+                    $agua_publica = 0;
+                    break;
+                default:
+                    $agua_publica = 0;
+            }
+
+            $fuente_agua = ($data['fuenteDeAgua'] == null ) ? '' :$data['fuenteDeAgua'];
+            
+
+
+
+
+
+
+
+
+
 
             echo $data['personas16a17'];
         }die;
