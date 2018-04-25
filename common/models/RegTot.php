c5f39617ef8be0c8e6e2c59fc4c09d10f72a33ba
joelsalgado <joelsalgado1302@gmail.com>
2018-04-25 15:06:07 -0500

    funcion arrays

--- common/models/RegTot.php
+++ common/models/RegTot.php
@@ -37,4 +37,38 @@ class RegTot extends \yii\db\ActiveRecord
             'no_vulnerable' => 'No Vulnerable',
         ];
     }
+
+
+    public static function getRegTot(array $val){
+        $suma1 = 0;
+        $suma2 = 0;
+        $suma3 = 0;
+        $suma4 = 0;
+        $suma5 = 0;
+        $model = self::find()->where(['id' => $val])->all();
+
+        foreach ($model as $value){
+            $suma1 = $suma1 + $value->pobreza_extrema;
+            $suma2 = $suma2 + $value->pobreza_moderada;
+            $suma3 = $suma3 + $value->vulnerable_por_ingresos;
+            $suma4 = $suma4 + $value->vulnerable_por_carencias;
+            $suma5 = $suma5 + $value->no_vulnerable;
+        }
+        $array1 = self::find()->where(['id' => $val])->asArray()->all();
+        $array2[] = [
+            'desc_region' => 'Total',
+            'id' => '',
+            'pobreza_extrema' => $suma1,
+            'pobreza_moderada' => $suma2,
+            'vulnerable_por_ingresos' => $suma3,
+            'vulnerable_por_carencias' => $suma4,
+            'no_vulnerable' => $suma5,
+        ];
+
+        $resultado = array_merge($array1, $array2);
+
+        return (object) $resultado;
+
+
+    }
 }
