3787c9e66602c821104dc309cd745b09eb1866d6
joelsalgado <joelsalgado1302@gmail.com>
2018-05-10 15:23:58 -0500

    Changes 2

--- frontend/assets/js/seguimiento.js
+++ frontend/assets/js/seguimiento.js
@@ -1,18 +1,38 @@
 $(document).ready(function() {
     piso = Number($('input:text[name="Seguimiento[acciones_piso]"]').val());
+    techo = Number($('input:text[name="Seguimiento[acciones_techo]"]').val());
     if(piso != 1){
         $('.piso').hide();
     }
+
+    if(techo != 1){
+        $('.techo').hide();
+    }
+
 });
 
 
-function fncSumar() {
+function fncPiso() {
     pisof = Number($('input:text[name="Seguimiento[acciones_piso]"]').val());
 
     if(pisof == 1){
         $('.piso').show();
     }else{
         $('.piso').hide();
+        $('input[name = "Seguimiento[inversion_piso]"]').val('');
+        $('input[name = "Seguimiento[fecha_inicio_techo]"]').val('');
+        $('input[name = "Seguimiento[fecha_entrega_techo]"]').val('');
+
     }
 
+}
+
+function fncTecho() {
+    techof = Number($('input:text[name="Seguimiento[acciones_techo]"]').val());
+
+    if(techof == 1){
+        $('.techo').show();
+    }else{
+        $('.techo').hide();
+    }
 }
