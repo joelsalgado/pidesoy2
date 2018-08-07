87f20a4251d732825b98dab1497028189e33117b
joelsalgado <joelsalgado1302@gmail.com>
2018-08-07 17:25:24 -0500

    cambios a formatos pdf bitacora y directorio

--- frontend/controllers/BitacoraReunionController.php
+++ frontend/controllers/BitacoraReunionController.php
@@ -128,15 +128,49 @@ class BitacoraReunionController extends Controller
                 'filename' => 'bitacora-reunion '.$model->id.'.pdf',
                 'marginLeft'=> 10,
                 'marginRight'=> 10,
-                'marginTop'=> 10,
+                'marginTop'=> 22,
                 'marginBottom'=> 13,
-                'orientation' => Pdf::FORMAT_A4,
+                'marginHeader'=> 5,
+                'orientation' => Pdf::ORIENT_LANDSCAPE,
                 'options' => [
                     'title' => 'Bitacora de Reuniones'
                 ],
                 'cssFile' => '@vendor/kartik-v/yii2-mpdf/assets/kv-mpdf-bootstrap.min.css',
                 'methods' => [
-                    'SetFooter' => ['|Pagina {PAGENO}|'],
+                    'SetHeader' => [
+                        '
+                            <table class="table table-condensed">
+                                <tr>
+                                    <td >
+                                        <img class="rounded float-left" src="'.Yii::$app->homeUrl.'images/escudo.png"  height="40" width="160">
+                                    </td>
+                                    <td align="center">
+                            
+                                    </td>
+                                    <td align="right">
+                                        <img style="text-align:right" src="'.Yii::$app->homeUrl.'images/edomex1.png" height="35" width="200">
+                                    </td>
+                                </tr>
+                            </table>
+                        
+                        ', 'line' => 1
+                    ],
+                    'SetFooter' => ['
+                        <table width="100%">
+                            <tr>
+                                <td width="25%"></td>
+                                <td width="50%" align="center"><p style="font-size: 5pt">{PAGENO}/{nbpg}</p></td>                            
+                                <td width="25%" align="right">
+                                    <p style="font-size: 5pt">Elaboró: CIEPS &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; Revisó: UDITI </p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="3" width="100%">
+                                    <img src="'.Yii::$app->homeUrl.'images/footer.png" height="20" width="1045"> 
+                                </td>
+                            </tr>
+                        </table>'
+                    ],
                 ]
             ]);
             return $pdf->render();
--- frontend/views/directorio-responsables/_reportView.php
+++ frontend/views/directorio-responsables/_reportView.php
@@ -7,83 +7,95 @@
  */
 
 ?>
-<table class="table table-condensed">
+<p style="color:#CC0066; font-size: medium"><b>FAMILIAS FUERTES, COMUNIDADES CON TODO</b></p>
+<table border="4" cellspacing="1" style="border-collapse: collapse" bordercolor="#111111" width="100%" height="100%">
     <tr>
-        <td>
-            <img class="rounded float-left" src="<?= Yii::$app->homeUrl ?>images/logoedo.png" height="75" width="75">
+        <td rowspan="2"  width="15%">
+            <img  src="<?= Yii::$app->homeUrl ?>images/colors.png" width="160" height="55">
         </td>
-        <td align="center">
-            <p align="center" style="font-size: small; color:#ff3d96;"><b>FAMILIAS FUERTES, COMUNIDADES CON TODO</b></p>
-            <p align="center" style="font-size: small; color:#ff3d96;"><b>DIRECTORIO DE RESPONSABLES Y ENLACES</b></p>
-        </td>
-        <td align="right">
-            <img style="text-align:right" src="<?= Yii::$app->homeUrl ?>images/logomex.png" height="75" width="75">
+        <td align="right" style="background-color: #CC0066; color: white;" width="85%">
+            <b><p>FICHA DE DATOS PERSONALES PARA</p></b>
         </td>
     </tr>
-</table>
+    <tr>
+        <td align="right" style="background-color: #CC0066; color: white;" width="85%">
+            <b><p>INTEGRAR EL DIRECTORIO DE RESPONSABLES Y OTROS</p></b>
+        </td>
+    </tr>
+</table><br>
 
 <table class="table table-bored">
     <tr>
-        <td align="right"><b>FECHA:</b> <?= $model->fecha = ($model->fecha)? Yii::$app->formatter->asDate($model->fecha, 'dd-MM-yyyy'): ''; ?></td>
+        <td colspan="12" align="right"><b>FECHA:</b> <?= $model->fecha = ($model->fecha)? Yii::$app->formatter->asDate($model->fecha, 'dd-MM-yyyy'): ''; ?></td>
+    </tr>
+    <tr>
+        <td colspan="6"><b>MUNICIPIO: </b><?= $model->mun->desc_mun ?></td>
+        <td colspan="6"><b>LOCALIDAD: </b><?= $model->loc->nombre_loc ?></td>
     </tr>
 </table>
 
-<p><b>TIPO DE PERSONAL (marque con una X)</p></b><br>
-<table class="table table-hover">
+<div class="alert alert-success-gray" role="alert">
+    <b>TIPO DE PERSONAL (marque con una X)</b>
+</div>
+
+<table  border="4" cellspacing="1" style="border-collapse: collapse" bordercolor="#111111" width="100%" height="100%">
     <tr>
-        <td><?= $otro = ($model->tipo_personal_id == 1) ? ' <b>(X)</b> ' : '( )' ?>Responsable Institucional</td>
-        <td><?= $otro = ($model->tipo_personal_id == 2) ? ' <b>(X)</b> ' : '( )' ?>Responsable Comunitario</td>
+        <td><?= $otro = ($model->tipo_personal_id == 1) ? ' <b>(X)</b> ' : '(&nbsp;&nbsp;&nbsp;) ' ?>Responsable Institucional</td>
+        <td><?= $otro = ($model->tipo_personal_id == 2) ? ' <b>(X)</b> ' : '(&nbsp;&nbsp;&nbsp;) ' ?>Responsable Comunitario</td>
     </tr>
     <tr>
-        <td><?= $otro = ($model->tipo_personal_id == 3) ? ' <b>(X)</b> ' : '( )' ?>Suplente del Responsable Institucional</td>
-        <td><?= $otro = ($model->tipo_personal_id == 4) ? ' <b>(X)</b> ' : '( )' ?>Suplente del Responsable Comunitario</td>
+        <td><?= $otro = ($model->tipo_personal_id == 3) ? ' <b>(X)</b> ' : '(&nbsp;&nbsp;&nbsp;) ' ?>Suplente del Responsable Institucional</td>
+        <td><?= $otro = ($model->tipo_personal_id == 4) ? ' <b>(X)</b> ' : '(&nbsp;&nbsp;&nbsp;) ' ?>Suplente del Responsable Comunitario</td>
     </tr>
     <tr>
-        <td><?= $otro = ($model->tipo_personal_id == 5) ? ' <b>(X)</b> ' : '( )' ?>Enlace del Responsable Institucional</td>
-        <td><?= $otro = ($model->tipo_personal_id == 6) ? ' <b>(X)</b> ' : '( )' ?>Persona para dejar recados al Responsable Comunitario</td>
+        <td><?= $otro = ($model->tipo_personal_id == 5) ? ' <b>(X)</b> ' : '(&nbsp;&nbsp;&nbsp;) ' ?>Enlace del Responsable Institucional</td>
+        <td><?= $otro = ($model->tipo_personal_id == 6) ? ' <b>(X)</b> ' : '(&nbsp;&nbsp;&nbsp;) ' ?>Persona para dejar recados al Responsable Comunitario</td>
     </tr>
+</table><br>
 
+<div class="alert alert-success-gray" role="alert">
+    <b>DATOS PERSONALES</b>
+</div>
 
-</table>
-
-<p><b>DATOS PERSONALES</p></b><br>
 <table class="table table-hover" >
 
     <tr>
-        <td colspan="2" rowspan="4">
+        <td colspan="2" rowspan="4" width="25%">
             <?php if ($model->imagen):?>
                 <img style="text-align:right" src="<?= Yii::$app->homeUrl ?>images/directorio/<?= $model->imagen ?>" height="120" width="120">
+            <?php else: ?>
+                <img style="text-align:right" src="<?= Yii::$app->homeUrl ?>images/sinimagen.jpg" height="120" width="120">
             <?php endif; ?>
-
         </td>
 
     </tr>
     <tr>
-        <td><b>NOMBRE:</b></td>
-        <td align="center"><u><?= $model->apellido_paterno ?></u></td>
-        <td align="center"><u><?= $model->apellido_materno ?></u></td>
-        <td align="center"><u><?= $model->nombre ?></u></td>
+        <td width="25%" align="center" style="border: black; border-left: black"><u><?= $model->apellido_paterno ?></u></td>
+        <td width="25%" align="center"><u><?= $model->apellido_materno ?></u></td>
+        <td width="25%" align="center"><u><?= $model->nombre ?></u></td>
     </tr>
     <tr>
-        <td></td>
-        <td align="center">Apellido Paterno</td>
-        <td align="center">Apellido Materno</td>
-        <td align="center">Nombre(s)</td>
+        <td width="25%" align="center"><b>Apellido Paterno</b></td>
+        <td width="25%" align="center"><b>Apellido Materno</b></td>
+        <td width="25%" align="center"><b>Nombre(s)</b></td>
     </tr>
+    <br>
     <tr>
-        <td><b>EDAD:</b><?= $edad ?></td>
-        <td><b>SEXO:</b> <?= $model->sexo ?></td>
-        <td colspan="2"><b>FECHA DE NACIMIENTO:</b> <?= $model->fecha_nacimiento = ($model->fecha_nacimiento)? Yii::$app->formatter->asDate($model->fecha_nacimiento, 'dd-MM-yyyy'): ''; ?></td>
+        <td align="center" width="20%"><b>EDAD:</b><?= $edad ?></td>
+        <td width="20%"><b>SEXO:</b> <?= $model->sexo ?></td>
+        <td width="35%"><b>FECHA DE NACIMIENTO:</b> <?= $model->fecha_nacimiento = ($model->fecha_nacimiento)? Yii::$app->formatter->asDate($model->fecha_nacimiento, 'dd-MM-yyyy'): ''; ?></td>
     </tr>
 </table>
 
-<br><p><b>DATOS PARA LOCALIZACIÓN.</p></b><br>
+<div class="alert alert-success-gray" role="alert">
+    <b>DATOS PARA LOCALIZACIÓN</b>
+</div>
 
 <table class="table table-striped">
     <tr>
         <td colspan="6"><b>CALLE:</b> <?= $model->calle ?></td>
         <td colspan="3"><b>NÚMERO EXTERIOR:</b> <?= $model->num_ext ?></td>
-        <td colspan="3"><b>NÚMERO INTERIOR:</b> <?= $model->num_int ?></td>
+        <td colspan="3" align="left"><b>NÚMERO INTERIOR:</b> <?= $model->num_int ?></td>
     </tr>
     <tr>
         <td colspan="6"><b>COLONIA: </b><?= $model->colonia ?></td>
@@ -94,9 +106,11 @@
         <td colspan="6"><b>TELÉFONO CELULAR: </b><?= $model->tel_cel ?></td>
     </tr>
     <tr>
-        <td colspan="4"><b>MUNICIPIO: </b><?= $model->mun->desc_mun ?></td>
-        <td colspan="4"><b>LOCALIDAD: </b><?= $model->loc->nombre_loc ?></td>
-        <td colspan="4"><b>REFERENCIA: </b><?= $model->referencia ?></td>
+        <td colspan="6"><b>MUNICIPIO: </b><?= $model->mun->desc_mun ?></td>
+        <td colspan="6"><b>LOCALIDAD: </b><?= $model->loc->nombre_loc ?></td>
+    </tr>
+    <tr>
+        <td colspan="12"><b>REFERENCIA: </b><?= $model->referencia ?></td>
     </tr>
     <tr>
         <td colspan="6"><b>CORREO ELECTRÓNICO: </b><?= $model->correo ?></td>
@@ -105,14 +119,14 @@
 </table>
 <br>
 
-<table class="table table-striped">
+<table class="table table-condensed">
     <tr>
-        <td align="center">__________________________________________________________</td>
+        <td align="center">_________________________________________________</td>
+        <td align="center">_________________________________________________</td>
     </tr>
     <tr>
-        <td align="center">HAGO CONSTAR QUE LA INFORMACIÓN ES VERDADERA</td>
-    </tr>
-    <tr>
-        <td align="center">Y ACEPTO LAS RESPONSABILIDADES <ASIGNADAS></ASIGNADAS></td>
+        <td align="center"><p style="font-size: 6pt">Nombre completo y firma del responsable de llenar el formato
+            </p> </td>
+        <td align="center"><p style="font-size: 6pt">Nombre completo y firma de la persona a la cual corresponden los datos de la ficha</p> </td>
     </tr>
 </table>
