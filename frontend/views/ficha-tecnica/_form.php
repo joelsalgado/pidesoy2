66872bf134283453b66df7d07cefd7ff5ce06182
Joel Salgado <marvinjoel_25@hotmail.com>
2018-08-28 17:51:03 -0500

    interrupcion sonia

--- frontend/views/ficha-tecnica/_form.php
+++ frontend/views/ficha-tecnica/_form.php
@@ -2,25 +2,123 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use kartik\select2\Select2;
+use kartik\depdrop\DepDrop;
+use yii\helpers\Url;
+use kartik\date\DatePicker;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\FichaTecnica */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="ficha-tecnica-form">
+<div class="container-fluid">
 
     <?php $form = ActiveForm::begin(); ?>
+    <div class="row">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="form-group">
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <?= $form->field($model, 'fecha')->widget(DatePicker::className(), [
+                        'name' => 'dp_2',
+                        'language' => 'es',
+                        'type' => DatePicker::TYPE_COMPONENT_PREPEND,
+                        //'readonly' => ($model->isNewRecord) ? false : true,
+                        'disabled' => ($model->isNewRecord) ? false : true,
+                        //'value' => 	date("d/m/Y", strtotime($model->fecha)),
+                        'pluginOptions' => [
+                            'autoclose'=>true,
+                            'format' => 'dd-mm-yyyy',
+                            'startDate' => '01-01-2017',
+                            'endDate' => '01-01-2020',
+                            //'value' => '22-10-1999'
+                        ]
+                    ]) ?>
+                </div>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'fecha')->textInput() ?>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <?= $form->field($model, 'entidad_id')->textInput(['readonly' => true, 'value' => 15]) ?>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <?= $form->field($model, 'region_id')->widget(Select2::classname(), [
+                        'data' => ArrayHelper::map(\common\models\Regiones::getRegionesOk(),
+                            'id', 'desc_region'),
+                        'options' => ['placeholder' => 'Selecciona una Region', 'id' => 'reg_id'],
+                        'pluginOptions' => [
+                            'allowClear' => true
+                        ],
+                    ]) ?>
+                </div>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'entidad_id')->textInput() ?>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <?=!$model->isNewRecord ? $form->field($model, 'mun_id')->widget(DepDrop::classname(), [
+                        'options' => ['id'=>'mun_id'],
+                        'type'=>DepDrop::TYPE_SELECT2,
+                        'data'=>[$model->mun_id=>$model->mun->nombre_mun],
+                        'select2Options'=>['pluginOptions'=>['allowClear'=>true]],
+                        'pluginOptions'=>[
+                            'depends'=>['reg_id'],
+                            'placeholder' => 'Selecciona un Municipio',
+                            'url' => Url::to(['solicitantes/municipios']),
+                        ]
+                    ]) :
+                        $form->field($model, 'mun_id')->widget(DepDrop::classname(), [
+                            'options' => ['id'=>'mun_id'],
+                            'type'=>DepDrop::TYPE_SELECT2,
+                            'select2Options'=>['pluginOptions'=>['allowClear'=>true]],
+                            'pluginOptions'=>[
+                                'depends'=>['reg_id'],
+                                'placeholder' => 'Selecciona un Municipio',
+                                'url' => Url::to(['solicitantes/municipios']),
+                            ]
+                        ])
 
-    <?= $form->field($model, 'region_id')->textInput() ?>
+                    ?>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="form-group">
 
-    <?= $form->field($model, 'mun_id')->textInput() ?>
-
-    <?= $form->field($model, 'loc_id')->textInput() ?>
+                    <?= !$model->isNewRecord ?  $form->field($model, 'loc_id')->widget(DepDrop::classname(), [
+                        'options' => ['id'=>'loc_id'],
+                        'data'=>[$model->loc_id=>$model->loc->desc_loc],
+                        'type'=>DepDrop::TYPE_SELECT2,
+                        'select2Options'=>['pluginOptions'=>['allowClear'=>true]],
+                        'pluginOptions'=>[
+                            'depends'=>['mun_id'],
+                            'placeholder' => 'Selecciona una Localidad',
+                            'url' => Url::to(['solicitantes/localidades']),
+                        ]
+                    ]) :
+                        $form->field($model, 'loc_id')->widget(DepDrop::classname(), [
+                            'options' => ['id'=>'loc_id'],
+                            'type'=>DepDrop::TYPE_SELECT2,
+                            'select2Options'=>['pluginOptions'=>['allowClear'=>true]],
+                            'pluginOptions'=>[
+                                'depends'=>['mun_id'],
+                                'placeholder' => 'Selecciona una Localidad',
+                                'url' => Url::to(['solicitantes/localidades']),
+                            ]
+                        ])
+                    ?>
+                </div>
+            </div>
+        </div>
 
     <?= $form->field($model, 'indicaciones')->textarea() ?>
 
@@ -93,7 +191,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'created_at')->textInput() ?>
 
     <?= $form->field($model, 'updated_at')->textInput() ?>
-
+</div>
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
